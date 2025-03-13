8f7e680273d8c54523921517d99221cbf3b183fe
OthoCrow <othocrow69@gmail.com>
2025-03-13 13:38:18 +1300

    did heaps

--- main.rs
+++ main.rs
@@ -6,11 +6,13 @@ fn main() {
         let mut text = String::new();//String for holding inputs
         stdin().read_line(&mut text).expect("Error with input"); //Take an input and append to "text"
         let nigs = text.trim();
+        let mut output = vec![];
         for c in nigs.chars(){
             let index: Option<usize> = alphabet.find(c);
             let shift:usize = 3;
             let shifted:usize = index.unwrap() + shift;
-            println!("{}", alphabet.chars().nth(shifted).unwrap());
+            output.push(alphabet.chars().nth(shifted).unwrap());
         }
-    }
+        println!("{:?}", output);
+    } 
 }
